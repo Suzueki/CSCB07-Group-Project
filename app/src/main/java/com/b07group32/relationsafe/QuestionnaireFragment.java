199fe76ad0954a51c7f079bf3ae5a68c20e242d1
axxuxu <amyxu006@gmail.com>
2025-07-21 13:21:35 -0400

    feat: add questionnaire UI layout

---
+++ app/src/main/java/com/b07group32/relationsafe/QuestionnaireFragment.java
@@ -0,0 +1,24 @@
+package com.b07group32.relationsafe;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import java.util.List;
+import java.util.List;
+public class QuestionnaireFragment extends Fragment {
+    private TextView questionText;
+    private TextView questionNumber;
+    private LinearLayout answerContainer;
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_questionnaire, container, false);
+    }
+}
