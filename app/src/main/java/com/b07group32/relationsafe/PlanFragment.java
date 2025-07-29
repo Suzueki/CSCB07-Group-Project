dba5bc155d252b5b58fd70f116e2be2b9f13c875
axxuxu <amyxu006@gmail.com>
2025-07-29 02:11:11 -0400

    feat: implemented RecyclerView for plan with sample data

--- app/src/main/java/com/b07group32/relationsafe/PlanFragment.java
+++ app/src/main/java/com/b07group32/relationsafe/PlanFragment.java
@@ -3,62 +3,46 @@ package com.b07group32.relationsafe;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PlanFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class PlanFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public PlanFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PlanFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PlanFragment newInstance(String param1, String param2) {
-        PlanFragment fragment = new PlanFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    ArrayList<PlanModel> plan = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_plan, container, false);
+        View view = inflater.inflate(R.layout.fragment_plan, container, false);
+        RecyclerView rv = view.findViewById(R.id.planRecyclerView);
+        setUpPlan();
+        return view;
+    }
+
+    private void setUpPlan() {
+        ArrayList<HashMap<String, String>> tips = new ArrayList<>();
+
+        // sample data (actual data will be from database)
+        HashMap<String, String> t1 = new HashMap<>();
+        t1.put("qid", "sr3");
+        t1.put("tip", "Save {answer} in your phone under a code-word so you can quietly call them when you need help.");
+        t1.put("answer", "mother");
+
+        HashMap<String, String> t2 = new HashMap<>();
+        t2.put("qid", "sr2");
+        t2.put("tip", "Log each incident (date, time, and a brief note) so you have clear records if needed later.");
+
+        tips.add(t2);
+        tips.add(t1);
+
+        for (int i=0; i<tips.size(); i++) {
+            HashMap<String, String> tip = tips.get(i);
+            plan.add(new PlanModel(tip.get("qid"), tip.get("tip"), tip.getOrDefault("answer", "")));
+        }
     }
 }
