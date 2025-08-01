6be070688bda62edf3ea6376ff399b1107de41e8
axxuxu <amyxu006@gmail.com>
2025-08-01 14:00:35 -0400

    feat: added AutoCompleteTextView to potentially replace Spinner, connected RecyclerView to adapter, and reformatted plan item

--- app/src/main/java/com/b07group32/relationsafe/PlanFragment.java
+++ app/src/main/java/com/b07group32/relationsafe/PlanFragment.java
@@ -2,18 +2,38 @@ package com.b07group32.relationsafe;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class PlanFragment extends Fragment {
     ArrayList<PlanModel> plan = new ArrayList<>();
+    DatabaseReference databaseReference;
+    FirebaseUser user;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        databaseReference = FirebaseDatabase.getInstance().getReference("users").child(user.getUid()); // add .child(list of tips)
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,6 +41,24 @@ public class PlanFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_plan, container, false);
         RecyclerView rv = view.findViewById(R.id.planRecyclerView);
         setUpPlan();
+        Plan_RecyclerViewAdapter adapter = new Plan_RecyclerViewAdapter(requireContext(), plan);
+        rv.setAdapter(adapter);
+        rv.setLayoutManager(new LinearLayoutManager(requireContext()));
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    // add to list
+                }
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
         return view;
     }
 
